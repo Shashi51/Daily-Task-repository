2a2606b5aa80eb0c08e352590681a5c14798c494
Shashi51 <111456876+Shashi51@users.noreply.github.com>
2022-09-07 18:47:16 +0530

    Add files via upload

---
+++ springboot-camel/src/main/java/com/javainuse/springbootcamel/SpringbootCamelApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse.springbootcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCamelApplication.class, args);
+	}
+
+}
---
+++ springboot-camel/src/test/java/com/javainuse/springbootcamel/SpringbootCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainuse.springbootcamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
