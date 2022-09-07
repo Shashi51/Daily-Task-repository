a5885631a7ac674e11a2396b92a7f0f2ffd7f055
Shashi51 <111456876+Shashi51@users.noreply.github.com>
2022-09-07 12:16:42 +0530

    Add files via upload

---
+++ springboot-swagger-test/src/test/java/com/example/springbootswaggertest/SpringbootSwaggerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
